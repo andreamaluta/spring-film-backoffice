45cb0358062e12b1adb451d8ef37e05a0d2ac2b8
andreamaluta <andreamaluta@gmail.com>
2025-09-25 10:48:43 +0200

    first commit

---
+++ src/main/java/org/lesson/java/spring_film_backoffice/SpringFilmBackofficeApplication.java
@@ -0,0 +1,13 @@
+package org.lesson.java.spring_film_backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFilmBackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFilmBackofficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lesson/java/spring_film_backoffice/SpringFilmBackofficeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lesson.java.spring_film_backoffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFilmBackofficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
