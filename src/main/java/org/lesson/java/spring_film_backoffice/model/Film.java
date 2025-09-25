2d7416021607c119af80b2ac05fea61dedf11798
andreamaluta <andreamaluta@gmail.com>
2025-09-25 11:59:58 +0200

    [UPD] made the Film entity

---
+++ src/main/java/org/lesson/java/spring_film_backoffice/model/Film.java
@@ -0,0 +1,109 @@
+package org.lesson.java.spring_film_backoffice.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "films")
+public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "The title must exist and cannot be blank, null or empty")
+    private String title;
+
+    @NotBlank(message = "The director must exist and cannot be blank, null or empty")
+    private String director;
+
+    @Lob
+    private String plot;
+
+    @NotNull(message = "The length of the film must exist and cannot be null or empty")
+    @Min(value = 60)
+    private short length;
+
+    @NotNull(message = "The publication date of the film must exist and cannot be null or empty")
+    private LocalDate publication_date;
+
+    @NotBlank(message = "The original language must exist and cannot be blank, null or empty")
+    private String original_language;
+
+    @NotBlank(message = "The poster must exist and cannot be blank, null or empty")
+    private String poster;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getPlot() {
+        return plot;
+    }
+
+    public void setPlot(String plot) {
+        this.plot = plot;
+    }
+
+    public short getLength() {
+        return length;
+    }
+
+    public void setLength(short length) {
+        this.length = length;
+    }
+
+    public LocalDate getPublication_date() {
+        return publication_date;
+    }
+
+    public void setPublication_date(LocalDate publication_date) {
+        this.publication_date = publication_date;
+    }
+
+    public String getOriginal_language() {
+        return original_language;
+    }
+
+    public void setOriginal_language(String original_language) {
+        this.original_language = original_language;
+    }
+
+    public String getPoster() {
+        return poster;
+    }
+
+    public void setPoster(String poster) {
+        this.poster = poster;
+    }
+
+}
