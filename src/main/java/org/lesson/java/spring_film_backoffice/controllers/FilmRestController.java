c8110d8d4d63104a72e66428cb3d314c00be541b
andreamaluta <andreamaluta@gmail.com>
2025-10-15 10:56:38 +0200

    [UPD]

--- src/main/java/org/lesson/java/spring_film_backoffice/controllers/FilmRestController.java
+++ src/main/java/org/lesson/java/spring_film_backoffice/controllers/FilmRestController.java
@@ -7,7 +7,6 @@ import org.lesson.java.spring_film_backoffice.model.Film;
 import org.lesson.java.spring_film_backoffice.service.FilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
